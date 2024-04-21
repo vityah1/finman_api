0800c608ead68275e09ba18f9b3b87c034894840
vityah1 <vholoshivskiy@gmail.com>
2024-04-21 13:27:05 +0300

    add logging to get rates remove ORM & loadenv from get rates

--- scripts/get_rates.py
+++ scripts/get_rates.py
@@ -1,39 +1,88 @@
 import datetime
 import logging
-import os
-import sys
+from os import environ, path
+from sys import path as syspath
+from urllib.parse import urlparse
 
-
-import dotenv
+import pymysql
 import requests
-from sqlalchemy import and_, create_engine, func
-from sqlalchemy.orm import sessionmaker
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from models import SprExchangeRates
+syspath.append(path.abspath(path.join(path.dirname(__file__), '..')))
 
-dotenv.load_dotenv()
+DATABASE_URI = environ["DATABASE_URI"]
 
-SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URI"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True, pool_size=10, pool_pre_ping=True)
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-Session = sessionmaker(bind=engine)
+    file_handler = logging.FileHandler('get_rates.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-logger = logging.getLogger()
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
+
+
+logger = setup_logging()
+
+
+def get_db_connection():
+    parsed_uri = urlparse(DATABASE_URI)
+    user = parsed_uri.username
+    password = parsed_uri.password
+    host = parsed_uri.hostname
+    database = parsed_uri.path[1:]
+    return pymysql.connect(host=host, user=user, password=password, db=database, cursorclass=pymysql.cursors.DictCursor)
 
 
 def get_rates_from_api():
     url = 'https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11'
     response = requests.get(url)
-    data = response.json()
-    return data
+    return response.json()
+
+
+def find_existing_rate(cursor, current_date, currency, base_currency):
+    cursor.execute(
+        """
+        SELECT * FROM spr_exchange_rates WHERE DATE(rdate) = %s AND currency = %s AND base_currency = %s
+        """, (current_date, currency, base_currency)
+    )
+    return cursor.fetchone()
+
+
+def update_rate(cursor, sale_rate, purchase_rate, existing_rate, currency):
+    cursor.execute(
+        """
+        UPDATE spr_exchange_rates SET saleRate = %s, purchaseRate = %s, updated = %s WHERE id = %s
+        """, (sale_rate, purchase_rate, datetime.datetime.now(datetime.timezone.utc), existing_rate['id'])
+    )
+    logger.info(
+        f"Updated {currency} rate: saleRate from {existing_rate['saleRate']} to {sale_rate}, purchaseRate from {existing_rate['purchaseRate']} to {purchase_rate}"
+    )
+
+
+def insert_new_rate(cursor, current_date, base_currency, currency, sale_rate, purchase_rate):
+    cursor.execute(
+        """
+        INSERT INTO spr_exchange_rates (rdate, base_currency, currency, saleRate, purchaseRate, created, updated, source)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, 'pryvat_api')
+        """, (current_date, base_currency, currency, sale_rate, purchase_rate,
+              datetime.datetime.now(datetime.timezone.utc), datetime.datetime.now(datetime.timezone.utc))
+    )
+    logger.info(f"Added new {currency} rate: saleRate {sale_rate}, purchaseRate {purchase_rate}")
 
 
 def update_or_create_rates():
     current_date = datetime.datetime.now(datetime.timezone.utc).date()
     rates_from_api = get_rates_from_api()
-    session = Session()
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    is_need_commit = False
 
     for rate in rates_from_api:
         currency = rate['ccy']
@@ -41,33 +90,21 @@ def update_or_create_rates():
         sale_rate = float(rate['sale'])
         purchase_rate = float(rate['buy'])
 
-        existing_rate = session.query(SprExchangeRates).filter(
-            and_(
-                func.date(SprExchangeRates.rdate) >= current_date,
-                SprExchangeRates.rdate < current_date + datetime.timedelta(days=1),
-                SprExchangeRates.currency == currency, SprExchangeRates.base_currency == base_currency
-            )
-        ).first()
+        existing_rate = find_existing_rate(cursor, current_date, currency, base_currency)
 
         if existing_rate:
-            if existing_rate.saleRate != sale_rate or existing_rate.purchaseRate != purchase_rate:
-                print(f"{existing_rate.saleRate=} != {sale_rate=} or {existing_rate.purchaseRate=} != {purchase_rate=}")
-                existing_rate.saleRate = sale_rate
-                existing_rate.purchaseRate = purchase_rate
-                existing_rate.updated = datetime.datetime.now(datetime.timezone.utc)
-                session.commit()
-                print(f"Updated {currency} rate.")
+            if existing_rate['saleRate'] != sale_rate or existing_rate['purchaseRate'] != purchase_rate:
+                update_rate(cursor, sale_rate, purchase_rate, existing_rate, currency)
+                is_need_commit = True
         else:
-            # Додавання нового запису, якщо на поточну дату його не знайдено
-            new_rate = SprExchangeRates(
-                rdate=current_date, base_currency=base_currency, currency=currency, saleRate=sale_rate,
-                purchaseRate=purchase_rate, created=datetime.datetime.now(datetime.timezone.utc),
-                updated=datetime.datetime.now(datetime.timezone.utc),
-                source='pryvat_api'
-            )
-            session.add(new_rate)
-            session.commit()
-            print(f"Added new {currency} rate.")
+            insert_new_rate(cursor, current_date, base_currency, currency, sale_rate, purchase_rate)
+            is_need_commit = True
+
+    if is_need_commit:
+        conn.commit()
+
+    cursor.close()
+    conn.close()
 
 
 if __name__ == '__main__':
